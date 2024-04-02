1a156a02b4caa9ad89be681900c15db1d5ba1507
riya <9048jj@naver.com>
2024-04-02 20:57:41 +0900

    first commit

---
+++ src/main/java/com/zerock/sendbox/SendBoxApplication.java
@@ -0,0 +1,13 @@
+package com.zerock.sendbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendBoxApplication.class, args);
+    }
+
+}
