d47f2a569822ea8e64a67504376380fc56db2108
riya <9048jj@naver.com>
2024-04-03 18:22:36 +0900

    commit

--- src/main/java/example/exampleController.java
+++ src/main/java/example/exampleController.java
@@ -3,13 +3,6 @@ package example;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class exampleController {
-
-
-    @GetMapping("/")
-    public String index() {
-
-        return "redirect:/guestbook/list";
-    }
-    asdg
+    
 
 }
