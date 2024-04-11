45949d5b482c94fe8cd190fbe9a917295311cb76
riya <9048jj@naver.com>
2024-04-11 19:21:27 +0900

    Chore : 테스트 하려다가 싹 고치게 된 것에 대하여

---
+++ src/main/java/com/zerock/sendbox/MainController.java
@@ -0,0 +1,19 @@
+package com.zerock.sendbox;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class MainController {
+
+    @GetMapping("/home")
+    public String selectImageList(Model model) {
+        model.addAttribute("title", "");
+        return "user/home";
+    }
+
+}
