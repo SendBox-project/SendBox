f28f662aeeb960d9f63e3e9ee65ca55f2b7c83e2
riya <9048jj@naver.com>
2024-04-12 13:27:13 +0900

    STYLE : account NG,OK K CSS 합침 및 ëMain Home Design

--- src/main/java/com/zerock/sendbox/MainController.java
+++ src/main/java/com/zerock/sendbox/MainController.java
@@ -13,7 +13,7 @@ public class MainController {
     @GetMapping("/home")
     public String selectImageList(Model model) {
         model.addAttribute("title", "");
-        return "user/home";
+        return "admin/member/modify_account_ng";
     }
 
 }
