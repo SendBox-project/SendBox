479edd89bdd723f2e381e76d549d51a7ff815dc1
riya <9048jj@naver.com>
2024-04-12 14:51:00 +0900

    Chore : 타임리프 레거시 코드 변경

--- src/main/java/com/zerock/sendbox/controller/user/UserMypageController.java
+++ src/main/java/com/zerock/sendbox/controller/user/UserMypageController.java
@@ -16,7 +16,8 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/user")
-public class UserMypageController {
+public class
+    UserMypageController {
     @Autowired
     UserMypageService userMypageService;
 
