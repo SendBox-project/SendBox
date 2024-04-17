cb555b021393ea540e05244cc7ea6979553df0ad
riya <9048jj@naver.com>
2024-04-17 15:16:51 +0900

    수정

--- src/main/java/com/zerock/sendbox/repository/RoomRepository.java
+++ src/main/java/com/zerock/sendbox/repository/RoomRepository.java
@@ -1,9 +1,7 @@
 package com.zerock.sendbox.repository;
 
 import com.zerock.sendbox.entity.Room;
-import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,12 +11,4 @@ public interface RoomRepository extends JpaRepository<Room, Integer> {
 
     // storeNo를 통해 한 업체가 갖고 있는 room 리스트를 가져옴
     List<Room> findByStoreStoreNo(Integer storeNo);
-    @Query("select r from Room r where r.store.storeNo =:storeNo ")
-    List<Room> findByStoreNo(@Param("storeNo") Integer storeNo);
-
-    //roomNo로 룸정보 가져오기
-    Room findByRoomNo(Integer roomNo);
-
-    Room findByRoomNoAndStore_StoreNo(Integer roomNo, Integer storeNo);
-
 }
