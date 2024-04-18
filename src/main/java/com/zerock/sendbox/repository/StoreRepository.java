69ad9d1eac4a18e14b1f948f09b7d602d8987495
riya <158006769+riyazzu@users.noreply.github.com>
2024-04-18 11:36:10 +0900

    Update StoreRepository.java

--- src/main/java/com/zerock/sendbox/repository/StoreRepository.java
+++ src/main/java/com/zerock/sendbox/repository/StoreRepository.java
@@ -1,7 +1,11 @@
 package com.zerock.sendbox.repository;
 
+import com.zerock.sendbox.entity.Orders;
 import com.zerock.sendbox.entity.Store;
+import feign.Param;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
