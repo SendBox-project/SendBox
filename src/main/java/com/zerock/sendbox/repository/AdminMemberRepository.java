8a91da3d433ea2c7c21e666ef53b783c1898108b
minki <mkhong916@gmail.com>
2024-04-18 16:39:30 +0900

    어드민 마이페이지 완료

--- src/main/java/com/zerock/sendbox/repository/AdminMemberRepository.java
+++ src/main/java/com/zerock/sendbox/repository/AdminMemberRepository.java
@@ -1,14 +1,30 @@
 package com.zerock.sendbox.repository;
 
 import com.zerock.sendbox.entity.AdminMember;
+import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface AdminMemberRepository extends JpaRepository<AdminMember, Integer> {
+
+    // 개인 정보 수정폼
     AdminMember findByAdminId(String adminId);
 
+    // 개인 정보 조회
+    AdminMember findByAdminNo(Integer adminNo);
+
+    //개인 정보 탈퇴
+    @Modifying //@쿼리 어노테이션(jpql쿼리, native쿼리)을 통해서 작성된 insert, update, delete 쿼리에서 사용됨!
+    @Query("update AdminMember set deleteYn = 'y' where adminNo =:adminNo")
+    Integer deleteInfo(@Param("adminNo")Integer adminNo);
+
+
     AdminMember findByMail(String mail);
+
+
 }
