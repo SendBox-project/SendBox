3c4b770101f813a2a978f4431768300dddb2f87f
riya <158006769+riyazzu@users.noreply.github.com>
2024-04-18 11:28:13 +0900

    Update SecurityConfig.java

--- src/main/java/com/zerock/sendbox/config/SecurityConfig.java
+++ src/main/java/com/zerock/sendbox/config/SecurityConfig.java
@@ -1,10 +1,10 @@
-package com.zerock.sendbox.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
@@ -31,5 +31,10 @@ public class SecurityConfig {
                         .anyRequest().permitAll())
                 .build();
     }
-}
 
+    // 비밀번호 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
