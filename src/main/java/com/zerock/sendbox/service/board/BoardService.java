87e57d6143a253699cda8c73d9c66834bf042427
riya <9048jj@naver.com>
2024-04-24 17:35:09 +0900

    커밋오류

--- src/main/java/com/zerock/sendbox/service/board/BoardService.java
+++ src/main/java/com/zerock/sendbox/service/board/BoardService.java
@@ -3,8 +3,6 @@ package com.zerock.sendbox.service.board;
 import com.zerock.sendbox.dto.board.BoardDTO;
 import com.zerock.sendbox.dto.board.PageRequestDTO;
 import com.zerock.sendbox.dto.board.PageResultDTO;
-import com.zerock.sendbox.entity.AdminMember;
-import com.zerock.sendbox.entity.Board;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface BoardService {
@@ -13,17 +11,10 @@ public interface BoardService {
 
     BoardDTO get(Integer boardNo);
 
-    BoardDTO entityToDTO(Board board, AdminMember adminMember, Long AdminAnswerCount);
-
     PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO);
 
     @Transactional
     void removeWithAdminAnswer(Integer boardNo);
 
     void modify(BoardDTO boardDTO);
-
-
-
-
-
 }
--- src/main/java/com/zerock/sendbox/service/board/BoardServiceImpl.java
+++ src/main/java/com/zerock/sendbox/service/board/BoardServiceImpl.java
@@ -3,8 +3,10 @@ package com.zerock.sendbox.service.board;
 import com.zerock.sendbox.dto.board.BoardDTO;
 import com.zerock.sendbox.dto.board.PageRequestDTO;
 import com.zerock.sendbox.dto.board.PageResultDTO;
-import com.zerock.sendbox.entity.*;
+import com.zerock.sendbox.entity.AdminMember;
+import com.zerock.sendbox.entity.Board;
 import com.zerock.sendbox.repository.AdminAnswerRepository;
+import com.zerock.sendbox.repository.AdminMemberRepository;
 import com.zerock.sendbox.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -22,52 +24,36 @@ public class BoardServiceImpl implements BoardService {
 
     private final BoardRepository boardRepository;
     private final AdminAnswerRepository adminAnswerRepository;
+    private final AdminMemberRepository adminMemberRepository;
 
     @Override
     public Integer register(BoardDTO dto) {
-
         log.info(dto);
 
-        AdminMember adminMember = AdminMember.builder().adminNo(dto.getAdminNo()).build();
-        Board board = Board.builder()
-                .boardNo(dto.getBoardNo())
-                .title(dto.getTitle())
-                .content(dto.getContent())
-                .thumbnail(dto.getThumbnail())
-                .boardType(dto.getBoardType())
-                .adminMember(adminMember)
-                .build();
-        return boardRepository.save(board).getBoardNo();
-    }
+        // BoardDTO를 이용하여 Board 엔티티 생성
+        Board board = dtoToEntity(dto);
 
-    @Override
-    public BoardDTO entityToDTO(Board board, AdminMember adminMember, Long AdminAnswerCount) {
+        // AdminMember 엔티티를 가져옴
+        AdminMember adminMember = adminMemberRepository.findById(dto.getAdminNo())
+                .orElseThrow(() -> new IllegalArgumentException("해당 AdminMember를 찾을 수 없습니다. AdminNo: " + dto.getAdminNo()));
 
-        BoardDTO boardDTO = BoardDTO.builder()
-                .boardNo(board.getBoardNo())
-                .title(board.getTitle())
-                .content(board.getContent())
-                .regDate(board.getRegDate())
-                .modDate(board.getModDate())
-                .writerName(adminMember.getName())
-                .AdminAnswerCount(AdminAnswerCount.intValue())
-                .build();
+        // Board 엔티티에 AdminMember를 설정
+        board.setAdminMember(adminMember);
 
-        return boardDTO;
+        // Board 엔티티 저장
+        boardRepository.save(board);
 
+        return board.getBoardNo();
     }
 
     @Override
     public PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO) {
-
         log.info(pageRequestDTO);
 
-        Function<Object[], BoardDTO> fn = (en -> entityToDTO((Board)en[0],(AdminMember) en[1],(Long)en[2]));
+        Function<Object[], BoardDTO> fn = (en -> entityToDTO((Board) en[0], (AdminMember) en[1], (Long) en[2]));
 
         Page<Object[]> result = boardRepository.getBoardWithAdminAnswerCount(
-                pageRequestDTO.getPageable(Sort.by("boardNo").descending())  );
-
-
+                pageRequestDTO.getPageable(Sort.by("boardNo").descending()));
 
         return new PageResultDTO<>(result, fn);
     }
@@ -76,29 +62,49 @@ public class BoardServiceImpl implements BoardService {
     public BoardDTO get(Integer boardNo) {
         Object result = boardRepository.getBoardByBoardNo(boardNo);
 
-        Object[] arr = (Object[])result;
+        Object[] arr = (Object[]) result;
 
-        return entityToDTO((Board)arr[0], (AdminMember)arr[1], (Long)arr[2]);
+        return entityToDTO((Board) arr[0], (AdminMember) arr[1], (Long) arr[2]);
     }
 
     @Transactional
     @Override
     public void removeWithAdminAnswer(Integer boardNo) {
         adminAnswerRepository.deleteByBoardNo(boardNo);
-
         boardRepository.deleteById(boardNo);
     }
 
     @Transactional
     @Override
     public void modify(BoardDTO boardDTO) {
-        Board board = boardRepository.getReferenceById(boardDTO.getBoardNo());
+        Board board = boardRepository.findById(boardDTO.getBoardNo())
+                .orElseThrow(() -> new IllegalArgumentException("해당 Board를 찾을 수 없습니다. BoardNo: " + boardDTO.getBoardNo()));
 
-        if(board != null) {
-            board.changeTitle(boardDTO.getTitle());
-            board.changeContent(boardDTO.getContent());
+        board.changeTitle(boardDTO.getTitle());
+        board.changeContent(boardDTO.getContent());
 
-            boardRepository.save(board);
-        }
+        boardRepository.save(board);
+    }
+
+    private Board dtoToEntity(BoardDTO dto) {
+        return Board.builder()
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .thumbnail(dto.getThumbnail())
+                .boardType(dto.getBoardType())
+                .build();
+    }
+
+    private BoardDTO entityToDTO(Board board, AdminMember adminMember, Long AdminAnswerCount) {
+        return BoardDTO.builder()
+                .boardNo(board.getBoardNo())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .regDate(board.getRegDate())
+                .modDate(board.getModDate())
+                .writerMail(adminMember.getMail())
+                .writerName(adminMember.getName())
+                .AdminAnswerCount(AdminAnswerCount.intValue())
+                .build();
     }
 }
